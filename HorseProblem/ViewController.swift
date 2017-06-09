9680288bdf90c39c9a8ae9336d4429d019d43062
Joshua Kaden <jkaden@doitt.nyc.gov>
2017-06-09 11:59:20 -0400

    Initial Commit

---
+++ HorseProblem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HorseProblem
+//
+//  Created by Kaden, Joshua on 6/9/17.
+//  Copyright © 2017 Kaden, Joshua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
